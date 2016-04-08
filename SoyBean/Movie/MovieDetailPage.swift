df9e849811bcac5d20decdae26af4375be69711b
chenli <ali322@gmail.com>
2016-04-08 21:03:29 +0800

    change collectionview

--- SoyBean/Movie/MovieDetailPage.swift
+++ SoyBean/Movie/MovieDetailPage.swift
@@ -22,15 +22,20 @@ class MovieDetailPage: UIViewController{
         }
     }
     
+    var casts:[Creator] = []{
+        didSet{
+            castsView.reloadData()
+        }
+    }
+    
     @IBOutlet weak var cover:UIImageView!
     @IBOutlet weak var movietitle:UILabel!
     @IBOutlet weak var rating:UILabel!
     @IBOutlet weak var countries:UILabel!
     @IBOutlet weak var durations:UILabel!
     @IBOutlet weak var year:UILabel!
-    @IBOutlet weak var casts:UILabel!
+    @IBOutlet weak var castsView:UICollectionView!
     @IBOutlet weak var summary:UITextView!
-    @IBOutlet weak var photos:UIImageView!
     
     var movieService = MovieService()
     override func viewDidLoad() {
@@ -39,7 +44,9 @@ class MovieDetailPage: UIViewController{
         if id != nil{
             movieService.movieDetail(id!)
         }
-        
+        castsView.dataSource = self
+        castsView.collectionViewLayout = CastViewLayout()
+        castsView.registerClass(CastCell.self, forCellWithReuseIdentifier: "CastCell")
     }
     
     func updateUI(){
@@ -52,12 +59,8 @@ class MovieDetailPage: UIViewController{
             })
             movietitle.text = _movie.title
             
-            var _casts:[String] = []
-            for actor in _movie.casts{
-                _casts.append(actor.name)
-            }
-            let _castsStr = _casts.joinWithSeparator("/")
-            casts.attributedText = attributedStringFor("演员:\(_castsStr)")
+            casts = _movie.casts
+            
             rating.attributedText = attributedStringFor("评分:\(_movie.rate)")
             year.attributedText = attributedStringFor("上映:\(_movie.year)")
             let _countriesStr = _movie.countries.joinWithSeparator("/")
@@ -65,22 +68,6 @@ class MovieDetailPage: UIViewController{
             let _durationsStr = _movie.durations.joinWithSeparator("/")
             durations.attributedText = attributedStringFor("片长:\(_durationsStr)")
             summary.text = "\(_movie.summary)"
-            
-            var _photos = [UIImage]()
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
-                var photodatas = [NSData]()
-                for _photo in _movie.photos{
-                    let photodata = NSData(contentsOfURL: NSURL(string: _photo.image)!)!
-                    photodatas.append(photodata)
-                }
-                dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                    for photodata in photodatas{
-                        _photos.append(UIImage(data: photodata)!)
-                    }
-                    self.photos.animationImages = _photos
-                })
-            })
-            photos.animationRepeatCount = 1
             
             self.navigationItem.title = _movie.title
         }
@@ -90,5 +77,60 @@ class MovieDetailPage: UIViewController{
         let attrStr = NSMutableAttributedString(string: labelText)
         attrStr.addAttribute(NSKernAttributeName, value: 5, range: NSRange.init(location: 2, length: 1))
         return attrStr
+    }
+}
+
+extension MovieDetailPage:UICollectionViewDataSource{
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return casts.count
+    }
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = castsView.dequeueReusableCellWithReuseIdentifier("CastCell", forIndexPath: indexPath) as! CastCell
+        cell.cast = casts[indexPath.item]
+        return cell
+    }
+}
+
+class CastViewLayout:UICollectionViewFlowLayout{
+    override init() {
+        super.init()
+        self.estimatedItemSize = CGSizeMake(80, 50)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+class CastCell:UICollectionViewCell{
+    var cast:Creator?{
+        didSet{
+            self.updateUI()
+        }
+    }
+    
+    var castBtn:UIButton = UIButton()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(castBtn)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        castBtn.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        castBtn.frame = self.bounds
+    }
+    
+    func updateUI(){
+        let attributedTitle = NSAttributedString(string: cast!.name, attributes: [
+            NSFontAttributeName:UIFont.systemFontOfSize(13)
+            ])
+        castBtn.setAttributedTitle(attributedTitle, forState: .Normal)
+        //        castBtn.setTitle(cast?.name, forState: .Normal)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init with coder")
     }
 }
